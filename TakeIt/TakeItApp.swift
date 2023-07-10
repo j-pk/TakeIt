6f4de0890cf26b5e48d6db9ca1cc35d2402b99d3
j-pk <parker.kirby@gmail.com>
2023-07-10 09:13:29 -0400

    Initial Commit

---
+++ TakeIt/TakeItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeItApp.swift
+//  TakeIt
+//
+//  Created by Parker on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TakeItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
